e4e20ff168986c8f3d83e5e432b3bcab2699bb0b
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2025-12-16 20:47:18 +0200

    initial commit

---
+++ user-service/src/test/java/com/kawashreh/ecommerce/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawashreh.ecommerce.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
