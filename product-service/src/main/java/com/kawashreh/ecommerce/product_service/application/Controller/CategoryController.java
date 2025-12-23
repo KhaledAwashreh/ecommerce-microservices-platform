3a3b58c1563034a8ea961cd19b9ddd7d92c6df36
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2025-12-23 22:58:56 +0200

    added mapping for endpoints

--- product-service/src/main/java/com/kawashreh/ecommerce/product_service/application/Controller/CategoryController.java
+++ product-service/src/main/java/com/kawashreh/ecommerce/product_service/application/Controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.kawashreh.ecommerce.product_service.domain.service.CategoryService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/categories")
@@ -16,17 +17,25 @@ public class CategoryController {
         this.service = service;
     }
 
-    @GetMapping("/")
+    @GetMapping()
     public List<Category> getAll() {
         return service.getAll();
     }
-    @GetMapping("/{}")
-    public List<Category> find(@PathVariable) {
-        return service.getAll();
+    @GetMapping("/{categoryId}")
+    public Category find(@PathVariable UUID categoryId) {
+        return service.find(categoryId);
     }
-
-    @PostMapping("/")
+    @GetMapping("/name/{categoryName}")
+    public Category findByName(@PathVariable String categoryName) {
+        return service.findByName(categoryName);
+    }
+    @PostMapping()
     public void create(@RequestBody Category category) {
         service.save(category);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public void delete(@PathVariable UUID categoryId) {
+        service.delete(categoryId);
     }
 }
