dcca68d93742274fadb4ad6b148e5857f2a34421
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2025-12-28 18:09:26 +0200

    added product Controller

---
+++ product-service/src/main/java/com/kawashreh/ecommerce/product_service/application/Controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.kawashreh.ecommerce.product_service.application.Controller;
+
+import com.kawashreh.ecommerce.product_service.domain.model.Category;
+import com.kawashreh.ecommerce.product_service.domain.model.Product;
+import com.kawashreh.ecommerce.product_service.domain.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/product")
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService productService) {
+        this.service = productService;
+    }
+
+    @GetMapping
+    public List<Product> get() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> findById(@PathVariable UUID productId) {
+        Product product = service.find(productId);
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        service.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> delete(@PathVariable UUID productId) {
+        service.delete(productId);
+        return ResponseEntity.noContent().build();
+    }
+}
