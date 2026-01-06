3ea17ae6cd7ae15a11d04aea170cbabd049b091c
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2026-01-06 23:53:35 +0200

    added naming server

---
+++ naming-server/src/test/java/com/kawashreh/ecommerce/user_service/naming_server/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawashreh.ecommerce.user_service.naming_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
