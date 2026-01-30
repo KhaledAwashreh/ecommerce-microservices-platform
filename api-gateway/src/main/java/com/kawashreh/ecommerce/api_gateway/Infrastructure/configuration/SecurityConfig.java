94fa9d79efec83488bfc3fca887dca2d17dfd243
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2026-01-30 19:28:05 +0200

    added caching dependencies

--- api-gateway/src/main/java/com/kawashreh/ecommerce/api_gateway/Infrastructure/configuration/SecurityConfig.java
+++ api-gateway/src/main/java/com/kawashreh/ecommerce/api_gateway/Infrastructure/configuration/SecurityConfig.java
@@ -8,8 +8,6 @@ import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import static org.springframework.security.config.web.server.SecurityWebFiltersOrder.AUTHENTICATION;
-
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
