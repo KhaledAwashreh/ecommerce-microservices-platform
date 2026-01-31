449fe729447dfaea1f555e0ad39727d09741f72d
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2026-01-31 18:19:05 +0200

    wip

--- product-service/src/main/java/com/kawashreh/ecommerce/product_service/Const/CacheConstants.java
+++ product-service/src/main/java/com/kawashreh/ecommerce/product_service/Const/CacheConstants.java
@@ -1,6 +1,7 @@
 package com.kawashreh.ecommerce.product_service.Const;
 
-public final class CacheConstants {
+public final class
+CacheConstants {
     public final static String product_by_id = "productById";
     public final static String PRODUCT_REVIEW_BY_USER_ID = "productReviewByUserId";
     public final static String PRODUCT_REVIEW_BY_PRODUCT_ID = "productReviewByProductId";
