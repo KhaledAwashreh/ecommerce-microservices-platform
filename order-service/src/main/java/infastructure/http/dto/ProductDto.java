a52a848e4b02cf9fe5534d5bbd05b520f8286238
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2026-02-09 22:49:54 +0200

    added product dto

--- order-service/src/main/java/infastructure/http/dto/ProductDto.java
+++ order-service/src/main/java/infastructure/http/dto/ProductDto.java
@@ -1,9 +1,19 @@
 package infastructure.http.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDto {
 
     private UUID id;
