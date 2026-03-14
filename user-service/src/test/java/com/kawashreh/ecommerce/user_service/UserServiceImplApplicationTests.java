5898e68dc42e4473c3c4ef4c118e0598ca86027a
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2026-03-14 23:44:09 +0200

    WP

--- user-service/src/test/java/com/kawashreh/ecommerce/user_service/UserServiceImplApplicationTests.java
+++ user-service/src/test/java/com/kawashreh/ecommerce/user_service/UserServiceImplApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceImplApplicationTests {
+class UserServiceImplApplicationTests extends BaseIntegrationTest{
 
 	@Test
 	void contextLoads() {
