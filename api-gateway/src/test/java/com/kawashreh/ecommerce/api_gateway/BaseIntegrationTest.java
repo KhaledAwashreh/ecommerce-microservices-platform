ff4bfa6f0a63013f7cde50ca1ecbb3bab19589d3
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2026-03-14 19:26:23 +0200

    WIP

---
+++ api-gateway/src/test/java/com/kawashreh/ecommerce/api_gateway/BaseIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.kawashreh.ecommerce.api_gateway;
+
+import org.junit.jupiter.api.AfterAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class BaseIntegrationTest {
+
+    @Container
+    static GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine")
+            .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+    }
+
+    @AfterAll
+    static void cleanup() {
+        if (redis != null && redis.isRunning()) {
+            redis.stop();
+        }
+    }
+}
