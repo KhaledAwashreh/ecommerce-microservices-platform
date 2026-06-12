5c8058e6a04d415df4c772390a0c28e9a0c44ab5
Khaled Awashreh <Khaled.m.awashreh@gmail.com>
2026-06-12 22:48:50 +0300

    adding additional exceptional handling

--- user-service/src/main/java/com/kawashreh/ecommerce/user_service/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/kawashreh/ecommerce/user_service/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.kawashreh.ecommerce.user_service.exception;
 
-import org.springframework.cglib.core.Local;
+import com.kawashreh.ecommerce.common.dto.ErrorResponse;
+import com.kawashreh.ecommerce.common.exceptions.DuplicateEntityException;
+import com.kawashreh.ecommerce.common.exceptions.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,15 +14,13 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex) {
-
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(NoSuchElementException ex) {
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
                 ex.getMessage(),
                 LocalDateTime.now()
         );
-
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
@@ -33,16 +34,23 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
-
     @ExceptionHandler(DuplicateEntityException.class)
     public ResponseEntity<ErrorResponse> handleDuplicateEntityException(DuplicateEntityException ex) {
-
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.CONFLICT.value(),
                 ex.getMessage(),
                 LocalDateTime.now()
         );
-
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAll(Exception ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "An unexpected error occurred",
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 }
